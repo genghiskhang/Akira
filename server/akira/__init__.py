28196b5e336636e8092fe394cda74e9e3646fa81
genghiskhang <vinhkhl1@umbc.edu>
2025-03-20 01:58:05 -0400

    update init file imports

--- server/akira/__init__.py
+++ server/akira/__init__.py
@@ -3,5 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def test():
-    return 'Test'
+def home():
+    return 'Akira'
+
+import akira.controllers
+from akira.models import db
